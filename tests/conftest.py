39bebb9eb875dd4345eb4ac4f915f3a732da3a54
bryan-garcia <bgarci26@ucsc.edu>
2023-09-08 16:44:25 -0700

    typo?

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,12 +14,11 @@ def pytest_configure(config):
     """
     Configure settings before running tests
     """
-    
     #
     # We might want conditional virtual displays. Configure this when
     # appropriate using the "--headless" flag above.
     # For now, let's stay headless always
     #
     os.system('Xvfb :1 -screen 0 1600x1200x16 &')
-    os.environ['DISPLAY']='1.0'
+    os.environ['DISPLAY']=':1.0'
 

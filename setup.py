e00eb304caa6647153841dd8f8e56e3f3a05fd3d
bryan-garcia <bgarci26@ucsc.edu>
2023-09-17 12:20:32 -0700

    Feature: Update package requirements

--- setup.py
+++ setup.py
@@ -28,22 +28,18 @@ setup(
 
     # This should be a valid email address corresponding to the author listed
     # above.
-    author_email='bgarci26@ucsc.edu, umansky1@llnl.gov',
+    author_email='garcia299@llnl.gov, umansky1@llnl.gov',
     classifiers=[
 	'Development Status :: 5 - Production/Stable',
         #'Intended Audience :: Any UEDGE users',
         'Topic :: Software Development',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
-	'Programming Language :: Python :: 3.9',
+	'Programming Language :: Python :: 3.9'
     ],
     keywords='grid, mesh, generator, tokamak, edge, plasma, efit, uedge',
     packages=find_packages(),
-    python_requires='>=3.5, <4',
+    python_requires='>=3.8, <4',
     install_requires=[
         'm2r2',
         'scipy >= 1.3.1',
